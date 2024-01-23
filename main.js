e52370ba1e81721c096168620b20ab52fef0c8ea
Master Vincent <vincent.samuelsson@hotmail.com>
2024-01-23 10:13:28 +0100

    added array.from

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const wmf = document.querySelector("#book-list li:nth-child(2) .name");
-console.log(wmf);
+// console.log(wmf);
+
+var boks = document.querySelector("#book-list li .name");
+// console.log(boks);
+
+boks = document.querySelectorAll("#book-list li .name");
+// console.log(boks);
+
+Array.from(boks).forEach(function (book) {
+  console.log(book);
+});
