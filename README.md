93c6fe2eceaaf238607a4645b850795906e879d8
Master Vincent <vincent.samuelsson@hotmail.com>
2024-01-23 10:31:22 +0100

    added how to change html and text content

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ fall - loops?
 Array.from(titles).forEach(function (item) {
 console.log(item);
 });
+
+const wmf = document.querySelector("#book-list li:nth-child(2) .name");
+// console.log(wmf);
+
+var boks = document.querySelector("#book-list li .name");
+// console.log(boks);
+
+boks = document.querySelectorAll("#book-list li .name");
+// console.log(boks);
+
+Array.from(boks).forEach(function (book) {
+console.log(book);
+});
--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const wmf = document.querySelector("#book-list li:nth-child(2) .name");
-// console.log(wmf);
-
-var boks = document.querySelector("#book-list li .name");
-// console.log(boks);
-
 boks = document.querySelectorAll("#book-list li .name");
 // console.log(boks);
 
 Array.from(boks).forEach(function (book) {
-  console.log(book);
+  book.textContent += "(book title)";
 });
+
+const bookList = document.querySelector("#book-list");
+// console.log(bookList.innerHTML);
+// bookList.innerHTML = "<h2> books is books </h2>";
+bookList.innerHTML += "<p> this is how you add HTML</p>";
